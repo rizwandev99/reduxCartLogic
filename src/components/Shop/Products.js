be58810327e44a9144e52b9f3650687a5583ac7f
rizwandev99 <rizwandev99@gmail.com>
2024-03-16 23:46:50 +0530

    hi

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 5,
+    title: "My first book",
+    description: "This is my first book",
+  },
+  {
+    id: "p2",
+    price: 10,
+    title: "My second book",
+    description: "This is my second book",
+  },
+];
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((item) => {
+          return (
+            <ProductItem
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              description={item.description}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
